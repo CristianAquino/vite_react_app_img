c252b9b890eab69c76eb722b426b6d07666c8d80
cristianaquino <ycristian10@gmail.com>
2023-01-13 17:20:39 -0500

    fear: adding redux

---
+++ src/redux/slices/authSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    token: JSON.parse(sessionStorage.getItem("token")),
+    loading: false,
+    error: false,
+    // user: JSON.parse(sessionStorage.getItem("user")),
+  },
+  reducers: {
+    // registerSuccess: (state, action) => {
+    //   const nuevo = action.payload;
+    //   sessionStorage.setItem("user", JSON.stringify(nuevo));
+    //   return {
+    //     ...state,
+    //     user: nuevo,
+    //     loading: false,
+    //     error: false,
+    //   };
+    // },
+    loginSuccess: (state, action) => {
+      sessionStorage.setItem("token", JSON.stringify(action.payload));
+      return {
+        ...state,
+        token: action.payload,
+        loading: false,
+        error: false,
+      };
+    },
+    // updateData: (state, action) => {
+    //   const nuevo = action.payload;
+    //   const a = { ...state.user, ...nuevo };
+    //   sessionStorage.setItem("user", JSON.stringify(a));
+    //   return {
+    //     ...state,
+    //     user: { ...state.user, ...nuevo },
+    //   };
+    // },
+    logout: (state) => {
+      sessionStorage.clear();
+      return {
+        ...state,
+        user: null,
+        token: null,
+      };
+    },
+  },
+});
+
+export default authSlice.reducer;
+
+export const { loginSuccess, registerSuccess, updateData, logout } =
+  authSlice.actions;
