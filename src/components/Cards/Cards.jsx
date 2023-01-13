e606c152aab805cb1014b9378b2d004b35acea9b
cristianaquino <ycristian10@gmail.com>
2023-01-13 17:15:50 -0500

    feat: cards design

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from "../Card/Card";
+import style from "./Cards.module.css";
+
+const Cards = () => {
+  return (
+    <div className={style.cards}>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default Cards;
