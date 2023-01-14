03fb66d150df8352983ac463912b5c4dd712d828
cristianaquino <ycristian10@gmail.com>
2023-01-14 15:56:14 -0500

    feat: adding Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 // rutas
 import routes from "./base/settings/routes";
+import { ProtectedRoute } from "./Routes/ProtectedRoute";
 
 // pages
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import Principal from "./Pages/Principal";
 
 function App() {
@@ -12,6 +14,9 @@ function App() {
     <Routes>
       <Route path={routes.HOME} element={<Principal />}>
         <Route index element={<Home />} />
+        <Route element={<ProtectedRoute isLogged={true} />}>
+          <Route path={routes.LOGIN} element={<Login />} />
+        </Route>
       </Route>
       <Route path="*" element={<Navigate replace to={routes.HOME} />} />
     </Routes>
