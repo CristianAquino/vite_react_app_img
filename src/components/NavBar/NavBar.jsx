a929d5f92394fad4aff15912c390704e700ec748
cristianaquino <ycristian10@gmail.com>
2023-01-16 17:25:46 -0500

    feat: adding functionality of menu icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,30 @@
 import styles from "./NavBar.module.css";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-
-// components
 import Logo from "../Logo/Logo";
-
-// states
-import { useState } from "react";
+import Menu from "../Menu/Menu";
+import { useMenu } from "../../hooks/useMenu";
 
 const NavBar = () => {
-  const [menuBar, setMenuBar] = useState(false);
+  const [isOpenMenu, openMenu, closeMenu] = useMenu();
 
   const handleChangeIconMenu = () => {
-    setMenuBar(!menuBar);
+    if (isOpenMenu) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
   };
+
   return (
-    <div className={styles.navBar}>
-      <Logo />
-      <div onClick={handleChangeIconMenu} className={styles.iconBars}>
-        {menuBar ? <AiOutlineClose /> : <AiOutlineMenu />}
+    <>
+      <div className={styles.navBar}>
+        <Logo />
+        <div onClick={handleChangeIconMenu} className={styles.iconBars}>
+          {isOpenMenu ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
       </div>
-    </div>
+      <Menu open={isOpenMenu} />
+    </>
   );
 };
 
