fa0407d19bb55f390ce2cbdce66e915b6948497d
cristianaquino <ycristian10@gmail.com>
2023-01-19 16:25:53 -0500

    feat: page Profile user

--- src/pages/ProfileUser.jsx
+++ src/pages/ProfileUser.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Profile } from "../components/Profile/Profile";
 
 const ProfileUser = () => {
-  return (
-    <div>
-      <Profile />
-    </div>
-  );
+  return <Profile />;
 };
 
 export default ProfileUser;
