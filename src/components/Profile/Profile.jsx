1842e2ddea8d4d85ef5b9775cf7baf7165ca1781
cristianaquino <ycristian10@gmail.com>
2023-01-31 23:16:54 -0500

    feat: renaming class of style in tags

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,6 @@ import { BiLock, BiEnvelope, BiUser, BiEdit } from "react-icons/bi";
 import { TbCameraPlus } from "react-icons/tb";
 import Image from "../CardImage/Image";
 
-import { IconContext } from "react-icons";
 import { useEditInput } from "../../hooks/useEditInput";
 
 export const Profile = () => {
@@ -27,100 +26,80 @@ export const Profile = () => {
 
   return (
     <div className={style.sesion}>
-      <div className={style.sesion__headS}>
-        <span className={style.sesion__title}>Perfil de usuario</span>
+      <div className={style.sesion__head}>
+        <span className="tag24bold">Perfil de usuario</span>
       </div>
-      <div className={style.sesion__body}>
+      <div className={stProfile.profile__body}>
         <div className={stProfile.changeImage}>
           <Image dimension={sizeImage} />
           <p className={stProfile.changeImage__iconContainer}>
-            <TbCameraPlus className={stProfile.icon} />
+            <TbCameraPlus className={`icon ${style.icon} `} />
           </p>
         </div>
         <form className={style.form} onSubmit={handleSubmit}>
           <div className={style.form__container}>
-            <span className={style.form__label}>Nombre de usuario</span>
+            <span className="tag16bold">Nombre de usuario</span>
             <div className={style.form__containerInput}>
-              <BiUser
-                className={`${style.form__icon} ${style["form__icon-left"]}`}
-              />
+              <BiUser className={`icon ${style.icon} ${style["icon--left"]}`} />
               <input
                 ref={refUsername}
                 type="text"
                 placeholder="username"
                 disabled
-                className={style.form__input}
+                className="form__input"
               />
-              <IconContext.Provider
-                value={{
-                  style: changeIconUsernameEdit
-                    ? ""
-                    : { color: "var(--description)" },
-                }}
-              >
-                <BiEdit
-                  onClick={LockInputUsername}
-                  className={`${style.form__icon} ${style["form__icon-right"]}`}
-                />
-              </IconContext.Provider>
+              <BiEdit
+                onClick={LockInputUsername}
+                className={`icon ${style.icon} ${style["icon--right"]} ${
+                  changeIconUsernameEdit ? "" : `${style["icon--off"]}`
+                }`}
+              />
             </div>
           </div>
           <div className={style.form__container}>
-            <span className={style.form__label}>Correo electronico</span>
+            <span className="tag16bold">Correo electronico</span>
             <div className={style.form__containerInput}>
               <BiEnvelope
-                className={`${style.form__icon} ${style["form__icon-left"]}`}
+                className={`icon ${style.icon} ${style["icon--left"]}`}
               />
               <input
                 ref={refEmail}
                 type="email"
                 placeholder="jhondoe@example.com"
                 disabled
-                className={style.form__input}
+                className="form__input"
               />
-              <IconContext.Provider
-                value={{
-                  style: changeIconEmailEdit
-                    ? ""
-                    : { color: "var(--description)" },
-                }}
-              >
-                <BiEdit
-                  onClick={LockInputEmail}
-                  className={`${style.form__icon} ${style["form__icon-right"]}`}
-                />
-              </IconContext.Provider>
+              <BiEdit
+                onClick={LockInputEmail}
+                className={`icon ${style.icon} ${style["icon--right"]} ${
+                  changeIconEmailEdit ? "" : `${style["icon--off"]}`
+                }`}
+              />
             </div>
           </div>
           <div className={style.form__container}>
-            <span className={style.form__label}>Contraseña</span>
+            <span className="tag16bold">Contraseña</span>
             <div className={style.form__containerInput}>
-              <BiLock
-                className={`${style.form__icon} ${style["form__icon-left"]}`}
-              />
+              <BiLock className={`icon ${style.icon} ${style["icon--left"]}`} />
               <input
                 // type={changeIconPasswordEdit ? "text" : "password"}
                 ref={refPassword}
                 type="text"
                 placeholder="********************"
                 disabled
-                className={style.form__input}
+                className="form__input"
               />
-              <IconContext.Provider
-                value={{
-                  style: changeIconEmailEdit
-                    ? ""
-                    : { color: "var(--description)" },
-                }}
-              >
-                <BiEdit
-                  onClick={LockInputPassword}
-                  className={`${style.form__icon} ${style["form__icon-right"]}`}
-                />
-              </IconContext.Provider>
+              <BiEdit
+                onClick={LockInputPassword}
+                className={`icon ${style.icon} ${style["icon--right"]}  ${
+                  changeIconPasswordEdit ? "" : `${style["icon--off"]}`
+                }`}
+              />
             </div>
           </div>
-          <button className={style.form__button}>guardar cambios</button>
+          <button className="form__button boton14medium">
+            guardar cambios
+          </button>
         </form>
       </div>
     </div>
