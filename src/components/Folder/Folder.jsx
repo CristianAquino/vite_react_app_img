9b4b4d59471ce1f83a58a6f405d3c29949d318d2
cristianaquino <ycristian10@gmail.com>
2023-02-02 18:04:20 -0500

    feat: renaming class of style in tags

--- src/components/Folder/Folder.jsx
+++ src/components/Folder/Folder.jsx
@@ -15,99 +15,136 @@ import { IoHeartCircleOutline } from "react-icons/io5";
 import { CgArrowsExchangeAltV } from "react-icons/cg";
 
 const Folder = () => {
-  const dimImge = {
-    borderRadius: "24px",
-  };
-
   return (
     <div className={style.folder}>
-      <div className={style.folder__title}>
+      <div className={`title32bold ${style["title32bold--var"]}`}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ad.
       </div>
-
       <div className={style.folder__body}>
-        <Image dimension={dimImge} />
-        <div className={style.folder__contentIcons}>
-          <AiFillStar className={style.folder__icon} />
-          <AiFillStar className={style.folder__icon} />
-          <AiFillStar className={style.folder__icon} />
-          <AiFillStar className={style.folder__icon} />
-          <AiFillStar className={style.folder__icon} />
+        <Image />
+        <div>
+          <AiFillStar className={style.iconStar} />
+          <AiFillStar className={style.iconStar} />
+          <AiFillStar className={style.iconStar} />
+          <AiFillStar className={style.iconStar} />
+          <AiFillStar className={style.iconStar} />
         </div>
         <div className={style.folder__info}>
           <div className={style.folder__tag}>
-            <GiAlliedStar className={style.folder__icon} />
-            <span className={style.folder__tag__data}>Ranking</span>
-            <span className={style.folder__tag__data}>average 4.5</span>
+            <GiAlliedStar className="icon" />
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              Ranking
+            </span>
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              average 4.5
+            </span>
           </div>
           <div className={style.folder__tag}>
-            <HiOutlineIdentification className={style.folder__icon} />
-            <span className={style.folder__tag__data}>other name</span>
-            <span className={style.folder__tag__data}>text</span>
+            <HiOutlineIdentification className="icon" />
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              other name
+            </span>
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              text
+            </span>
           </div>
           <div className={style.folder__tag}>
-            <BiUserCircle className={style.folder__icon} />
-            <span className={style.folder__tag__data}>author</span>
-            <span className={style.folder__tag__data}>author</span>
+            <BiUserCircle className="icon" />
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              author
+            </span>
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              author
+            </span>
           </div>
           <div className={style.folder__tag}>
-            <BsBrush className={style.folder__icon} />
-            <span className={style.folder__tag__data}>artist</span>
-            <span className={style.folder__tag__data}>artist</span>
+            <BsBrush className="icon" />
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              artist
+            </span>
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              artist
+            </span>
           </div>
           <div className={style.folder__tag}>
-            <BiRadioCircleMarked className={style.folder__icon} />
-            <span className={style.folder__tag__data}>status</span>
+            <BiRadioCircleMarked className="icon" />
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              status
+            </span>
             <span
-              className={`${style.folder__tag__data} ${style["folder__tag__data--variant"]}`}
+              className={`tag14medium ${style["tag14medium--var"]} ${style["tag14medium--var__color"]}`}
             >
               on going
             </span>
           </div>
           <div className={style.folder__tag}>
-            <BsTag className={style.folder__icon} />
-            <span className={style.folder__tag__data}>tags</span>
-            <span className={style.folder__tag__data}>tags</span>
+            <BsTag className="icon" />
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              tags
+            </span>
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              tags
+            </span>
           </div>
           <div className={style.folder__tag}>
-            <AiOutlineEye className={style.folder__icon} />
-            <span className={style.folder__tag__data}>view</span>
-            <span className={style.folder__tag__data}>123213213</span>
+            <AiOutlineEye className="icon" />
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              view
+            </span>
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              123213213
+            </span>
           </div>
           <div className={style.folder__tag}>
-            <IoHeartCircleOutline className={style.folder__icon} />
-            <span className={style.folder__tag__data}>follow</span>
-            <AiOutlineLike className={style.folder__icon} />
+            <IoHeartCircleOutline className="icon" />
+            <span className={`tag14medium ${style["tag14medium--var"]}`}>
+              follow
+            </span>
+            <AiOutlineLike className={style.iconFollow} />
           </div>
         </div>
-        <div className={style.folder__sumary}>
-          <div className={style.folder__sumary__head}>
-            <HiOutlineBookOpen className={style.folder__icon} />
-            <span className={style.folder__sumary__title}>sumary</span>
+        <div className={style.folderSumary}>
+          <div className={style.folderSumary__head}>
+            <HiOutlineBookOpen className="icon" />
+            <span className={`tag24bold ${style["tag24bold--var"]}`}>
+              sumary
+            </span>
           </div>
-          <div className={style.folder__sumary__body}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
-            suscipit.
-          </div>
+          <span
+            className={`description14medium ${style["description14medium--var"]}`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            error, quis laudantium fugiat iste velit, recusandae nam eius
+            eveniet quam eos at in! Recusandae ullam aperiam quo quis porro
+            expedita, accusamus eligendi perferendis at delectus! Perspiciatis
+            tempora, in earum recusandae eos magni quos ratione numquam,
+            debitis, eligendi maiores vero. Aliquid..
+          </span>
         </div>
-        <div className={style.folder__chapter}>
-          <div className={style.folder__chapter__head}>
-            <div style={{ display: "flex", gap: "8px", alignItems: "center" }}>
-              <AiOutlineUnorderedList className={style.folder__icon} />
-              <span className={style.folder__chapter__title}>chapters</span>
-            </div>
-            <CgArrowsExchangeAltV className={style.folder__icon} />
-          </div>
-          <div className={style.folder__chapter__body}>
-            <div className={style.folder__ch}>
-              <span>chapter 2</span>
-              <AiOutlineHeart className={style.folder__icon} />
-            </div>
-            <div className={style.folder__ch}>
-              <span>chapter 1</span>
-              <AiOutlineHeart className={style.folder__icon} />
-            </div>
-          </div>
+      </div>
+      <div className={style.folderChapter}>
+        <div className={style.folderChapter__head}>
+          <AiOutlineUnorderedList className="icon" />
+          <span className={`tag24bold ${style["tag24bold--var"]}`}>
+            chapters
+          </span>
+          <CgArrowsExchangeAltV className={`icon ${style["icon--cursor"]}`} />
+        </div>
+        <div className={style.folder__ch}>
+          <span className={`tag16medium ${style["tag16medium--var"]}`}>
+            chapter 2
+          </span>
+          <AiOutlineHeart
+            className={`${style.iconLike} ${style["icon--var"]}`}
+          />
+        </div>
+        <div className={style.folder__ch}>
+          <span className={`tag16medium ${style["tag16medium--var"]}`}>
+            chapter 1
+          </span>
+          <AiOutlineHeart
+            className={`${style.iconLike} ${style["icon--var"]}`}
+          />
         </div>
       </div>
     </div>
