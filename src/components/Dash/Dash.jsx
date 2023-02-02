f0ccb687e7ec31f2e6f9a77f1df99dfff3d4a812
cristianaquino <ycristian10@gmail.com>
2023-02-02 16:54:39 -0500

    feat: renaming class of style in tags

--- src/components/Dash/Dash.jsx
+++ src/components/Dash/Dash.jsx
@@ -4,20 +4,27 @@ import Image from "../CardImage/Image";
 
 const Dash = () => {
   const contenido = true;
+  const styleContainerImage = {
+    cursor: "pointer",
+  };
+
   return (
     <div className={style.dash__container}>
-      <div className={style.dash__header}>
-        <span className={style.dash__title}>Mis folders creados</span>
-        <AiOutlinePlus className={style.dash__icon} />
+      <div className={style.dash__head}>
+        <span className={`tag24bold ${style["tag24bold--var"]}`}>
+          Mis folders creados
+        </span>
+        <AiOutlinePlus className={`icon ${style["icon--var"]}`} />
       </div>
-
       <div className={style.dash__body}>
         {contenido ? (
           <div className={style["dash__body--grid"]}>
-            <Image />
+            <Image styleContainerImage={styleContainerImage} />
           </div>
         ) : (
-          <span className={style.dash__msg}>
+          <span
+            className={`description14medium ${style["description14medium--var"]}`}
+          >
             Usted no tiene folders creados
           </span>
         )}
