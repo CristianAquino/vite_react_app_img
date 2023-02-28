0f533e7635a59d8a526868bed873977ebe5f5860
cristianaquino <ycristian10@gmail.com>
2023-02-27 19:53:52 -0500

    feat: adding store of userSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
+import userSlice from "./slices/userSlice";
 
 export default configureStore({
   reducer: {
     authSlice,
+    userSlice,
   },
 });
