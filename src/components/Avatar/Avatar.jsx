2b861e0daae307ee0852456452de48ebbf3a873f
cristianaquino <ycristian10@gmail.com>
2023-02-27 19:59:16 -0500

    feat: removing backgroundColor functionality  in component Avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,22 +1,10 @@
-import { useEffect, useState } from "react";
+// style
 import style from "./Avatar.module.css";
-const Avatar = () => {
-  const [color, setColor] = useState("");
 
-  useEffect(() => {
-    const numeros = "0123456789abcdef";
-    const longitud = 6;
-    let initial = "";
-    for (let i = 0; i < longitud; i++) {
-      let random = Math.floor(Math.random() * numeros.length);
-      initial += numeros.substring(random, random + 1);
-    }
-    setColor("#" + initial);
-  }, []);
-
+const Avatar = ({ styleAvatar, name }) => {
   return (
-    <div className={style.avatar} style={{ backgroundColor: color }}>
-      C
+    <div className={style.avatar} style={{ ...styleAvatar }}>
+      {name?.split("")[0].toUpperCase()}
     </div>
   );
 };
