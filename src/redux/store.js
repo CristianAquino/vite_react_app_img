eacae368f93b51270bf51f890ec9618b126b46d7
cristianaquino <ycristian10@gmail.com>
2023-03-18 19:11:19 -0500

    feat: adding store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import userSlice from "./slices/userSlice";
+import folderSlice from "./slices/folderSlice";
+import chapterSlice from "./slices/chapterSlice";
 
 export default configureStore({
   reducer: {
     authSlice,
     userSlice,
+    folderSlice,
+    chapterSlice,
   },
 });
