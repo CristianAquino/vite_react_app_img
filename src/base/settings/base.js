a95bbb572d001ab9d7d0bf74e7d766853267cc3a
cristianaquino <ycristian10@gmail.com>
2023-03-26 22:26:10 -0500

    feat: adding new url

--- src/base/settings/base.js
+++ src/base/settings/base.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api1node-production.up.railway.app/api/",
+  // baseURL: "https://api1node-production.up.railway.app/api/",
+  baseURL: "https://apimocha.com/v1.1/test/",
 });
 
 export default instance;
