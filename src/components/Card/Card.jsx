8835ea5c10eb01059a67be47f93acbddc14c7e7c
cristianaquino <ycristian10@gmail.com>
2023-03-26 22:24:37 -0500

    feat: add and remove methods in card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,15 @@
 import CardImage from "../CardImage/CardImage";
 import CardData from "../CardData/CardData";
 import style from "./Card.module.css";
+import { Link } from "react-router-dom";
 
-const Card = () => {
+const Card = ({ data }) => {
   return (
     <div className={style.card}>
-      <CardImage />
-      <CardData />
+      <Link to={`folder/${data.title}`}>
+        <CardImage data={data} />
+      </Link>
+      <CardData data={data} />
     </div>
   );
 };
