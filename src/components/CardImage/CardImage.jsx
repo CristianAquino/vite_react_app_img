10d7ffbc2365b3112019b4f096c4e92f7a8ca612
cristianaquino <ycristian10@gmail.com>
2023-03-26 22:24:12 -0500

    feat: add and remove methods in cardImage

--- src/components/CardImage/CardImage.jsx
+++ src/components/CardImage/CardImage.jsx
@@ -1,52 +1,46 @@
-import { useState } from "react";
 import style from "./CardImage.module.css";
 import Image from "./Image";
 
-const CardImage = () => {
-  const [hoy, setHoy] = useState(new Date().getDate());
-  const [pub, setPub] = useState("");
-
+const CardImage = ({ data }) => {
   const styleContainerImage = {
     cursor: "pointer",
   };
-  const dato = {
-    f: "12/01/2020",
-    e: "",
-  };
-  // const hoy = new Date().getDate();
-  // const pub = dato.f.split("/")[0];
-  if (pub === "") {
-    setPub(dato.f.split("/")[0]);
-  }
 
-  const handleState = (dato) => {
-    const { f, e } = dato;
-    if ((e !== "end") & (hoy <= pub)) {
+  const status = () => {
+    const date = data.createdAt;
+    let day = handleTime(date);
+    if (day <= 72) {
       return (
-        <p
-          className={`tag8bold ${style["tag8bold--var"]} ${style["tag8bold--new"]}`}
-        >
-          Nuevo
-        </p>
+        <div className={`${style.stateFolder} ${style["stateFolder--new"]}`}>
+          <p
+            className={`tag8bold ${style["tag8bold--var"]} ${style["tag8bold--new"]}`}
+          >
+            Nuevo
+          </p>
+        </div>
       );
-    } else if (e === "end") {
-      return <p className={`tag8bold ${style["tag8bold--var"]}`}>Completo</p>;
+    } else if (data.status == 1) {
+      return (
+        <div className={`${style.stateFolder}  ${style["stateFolder--end"]}`}>
+          <p className={`tag8bold ${style["tag8bold--var"]}`}>Completo</p>
+        </div>
+      );
     }
   };
 
+  const handleTime = (chapter) => {
+    let hoy = new Date();
+    let apiTime = new Date(chapter);
+    let diferencia = hoy.getTime() - apiTime.getTime();
+    let days = Math.floor(diferencia / 1000 / 60 / 60);
+    return days;
+  };
   return (
-    <Image styleContainerImage={styleContainerImage}>
+    <Image styleContainerImage={styleContainerImage} url={data.img}>
       <p className={`tag12regular ${style["tag12regular--chapter"]}`}>
-        Chapter 101
+        {data.lastChapters[0].name}
       </p>
-      <div
-        className={`${style.stateFolder} ${
-          hoy <= pub ? `${style["stateFolder--new"]}` : ""
-        } ${dato.e === "end" ? `${style["stateFolder--end"]}` : ""}`}
-      >
-        {/* {hoy <= pub ? <p>New</p> : <p>Completo</p>} */}
-        {handleState(dato)}
-      </div>
+      {status()}
     </Image>
   );
 };
