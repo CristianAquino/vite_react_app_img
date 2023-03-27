00ec4f28069e4e5b1ea1b0c27bd060feaf4a4e14
cristianaquino <ycristian10@gmail.com>
2023-03-26 22:23:23 -0500

    feat: add and remove methods in register

--- src/components/SignIn/Register.jsx
+++ src/components/SignIn/Register.jsx
@@ -1,16 +1,99 @@
+// react
+import { useEffect, useState } from "react";
+// style
 import style from "./SingIn.module.css";
-import { Link } from "react-router-dom";
+// component
 import Logo from "../Logo/Logo";
+// icons
 import { BiLock, BiEnvelope, BiShow, BiHide, BiUser } from "react-icons/bi";
 import { FcGoogle } from "react-icons/fc";
+// hook
 import { useChangeIcon } from "../../hooks/useChangeIcon";
+// react-router-dom
+import { Link, useNavigate } from "react-router-dom";
+// react-hook-form
+import { useForm } from "react-hook-form";
+// redux
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import {
+  authStart,
+  loginSuccess,
+  authFail,
+} from "../../redux/slices/authSlice";
+import { userSuccess } from "../../redux/slices/userSlice";
+// http
+import { signup } from "../../https/authRequests";
 
 export const Register = () => {
+  // form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+
+    setError,
+  } = useForm({
+    defaultValues: {
+      firstname: "",
+      lastname: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const [color, setColor] = useState("");
   const [unlockPassword, setUnLockPassword] = useChangeIcon();
   const [unlockConfirmPassword, setUnLockConfirmPassword] = useChangeIcon();
+  // redux
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isLogged, error, message, loading } = useSelector(
+    (state) => state.authSlice
+  );
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    const numeros = "0123456789abcdef";
+    const longitud = 6;
+    let initial = "";
+    for (let i = 0; i < longitud; i++) {
+      let random = Math.floor(Math.random() * numeros.length);
+      initial += numeros.substring(random, random + 1);
+    }
+    setColor("#" + initial);
+  }, []);
+
+  useEffect(() => {
+    if (isLogged) {
+      navigate("/", { replace: true });
+    }
+  }, [isLogged]);
+
+  const onSubmit = (datos) => {
+    handleRegister(datos);
+  };
+
+  const handleRegister = async (datos) => {
+    // color debe destrucutararse para guardarse correctamente
+    datos.color = color;
+    if (datos.password === datos.confirmPassword) {
+      dispatch(authStart());
+      const { confirmPassword, ...data } = datos;
+      try {
+        const { token, user } = await signup(data);
+        dispatch(loginSuccess(token));
+        dispatch(userSuccess(user));
+      } catch (e) {
+        dispatch(authFail(e.response.data));
+      }
+    } else {
+      setError("confirmPassword", {
+        type: "custom",
+        message: "no hay coincidencia en esta contraseña",
+      });
+    }
   };
 
   return (
@@ -28,24 +111,116 @@ export const Register = () => {
       </div>
       <span className={`tag24bold ${style["tag24bold--var"]}`}>- or -</span>
       <div className={style.sesion__body}>
-        <form className={style.form} onSubmit={handleSubmit} autoComplete="off">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={style.form}
+          autoComplete="off"
+        >
           <div className={style.form__container}>
-            <span className={`tag16bold ${style["tag16bold--var"]}`}>
+            <p
+              className={`tag16bold ${style["tag16bold--var"]} ${style.form__titleInput}`}
+            >
+              Escriba su nombre
+            </p>
+            <div className={style.form__containerInput}>
+              <BiUser className={`icon ${style.icon} ${style["icon--left"]}`} />
+              <input
+                type="text"
+                placeholder="firstname"
+                className="form__input"
+                {...register("firstname", {
+                  required: "*Este campo es requerido",
+                  minLength: {
+                    value: 3,
+                    message: "debe ingresar al menos 3 caracteres",
+                  },
+                  maxLength: {
+                    value: 16,
+                    message: "debe ingresar menos de 16 caracteres",
+                  },
+                  pattern: {
+                    value: /[a-zA-Z]/,
+                    message: "Solo puede ingresar letras",
+                  },
+                })}
+              />
+            </div>
+            {errors.firstname && (
+              <span className="danger">{errors.firstname?.message}</span>
+            )}
+          </div>
+          <div className={style.form__container}>
+            <p
+              className={`tag16bold ${style["tag16bold--var"]} ${style.form__titleInput}`}
+            >
+              Escriba sus apellidos
+            </p>
+            <div className={style.form__containerInput}>
+              <BiUser className={`icon ${style.icon} ${style["icon--left"]}`} />
+              <input
+                type="text"
+                placeholder="lastname"
+                className="form__input"
+                {...register("lastname", {
+                  required: "*Este campo es requerido",
+                  minLength: {
+                    value: 3,
+                    message: "debe ingresar al menos 3 caracteres",
+                  },
+                  maxLength: {
+                    value: 16,
+                    message: "debe ingresar menos de 16 caracteres",
+                  },
+                  pattern: {
+                    value: /[a-zA-Z]/,
+                    message: "Solo puede ingresar letras",
+                  },
+                })}
+              />
+            </div>
+            {errors.lastname && (
+              <span className="danger">{errors.lastname?.message}</span>
+            )}
+          </div>
+          <div className={style.form__container}>
+            <p
+              className={`tag16bold ${style["tag16bold--var"]} ${style.form__titleInput}`}
+            >
               Escriba su usuario
-            </span>
+            </p>
             <div className={style.form__containerInput}>
               <BiUser className={`icon ${style.icon} ${style["icon--left"]}`} />
               <input
                 type="text"
                 placeholder="username"
                 className="form__input"
+                {...register("username", {
+                  required: "*Este campo es requerido",
+                  minLength: {
+                    value: 3,
+                    message: "debe ingresar al menos 3 caracteres",
+                  },
+                  maxLength: {
+                    value: 16,
+                    message: "debe ingresar menos de 16 caracteres",
+                  },
+                  pattern: {
+                    value: /[a-zA-Z0-9]/,
+                    message: "Solo puede ingresar numeros y letras",
+                  },
+                })}
               />
             </div>
+            {errors.username && (
+              <span className="danger">{errors.username?.message}</span>
+            )}
           </div>
           <div className={style.form__container}>
-            <span className={`tag16bold ${style["tag16bold--var"]}`}>
+            <p
+              className={`tag16bold ${style["tag16bold--var"]} ${style.form__titleInput}`}
+            >
               Ingrese su email
-            </span>
+            </p>
             <div className={style.form__containerInput}>
               <BiEnvelope
                 className={`icon ${style.icon} ${style["icon--left"]}`}
@@ -54,19 +229,48 @@ export const Register = () => {
                 type="text"
                 placeholder="jhondoe@example.com"
                 className="form__input"
+                {...register("email", {
+                  required: "*Este campo es requerido",
+                  pattern: {
+                    value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$/,
+                    message: "Debe ingresar un email valido",
+                  },
+                })}
               />
             </div>
+            {errors.email && (
+              <span className="danger">{errors.email?.message}</span>
+            )}
           </div>
           <div className={style.form__container}>
-            <span className={`tag16bold ${style["tag16bold--var"]}`}>
+            <p
+              className={`tag16bold ${style["tag16bold--var"]}  ${style.form__titleInput}`}
+            >
               Ingrese su contraseña
-            </span>
+            </p>
             <div className={style.form__containerInput}>
               <BiLock className={`icon ${style.icon} ${style["icon--left"]}`} />
               <input
                 type={unlockPassword ? "text" : "password"}
                 placeholder="********************"
                 className="form__input"
+                {...register("password", {
+                  required: "*Este campo es requerido",
+                  minLength: {
+                    value: 8,
+                    message: "debe ingresar al menos 8 caracteres",
+                  },
+                  maxLength: {
+                    value: 32,
+                    message: "debe ingresar menos de 32 caracteres",
+                  },
+                  pattern: {
+                    value:
+                      /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}:;<>,.?/~_+-=|]).{8,32}$/,
+                    message:
+                      "La contraseña tiene que incluir almenos un numero, una letra mayuscula, una minuscula y un caracter especial",
+                  },
+                })}
               />
               {unlockPassword ? (
                 <BiShow
@@ -80,17 +284,25 @@ export const Register = () => {
                 />
               )}
             </div>
+            {errors.password && (
+              <span className="danger">{errors.password?.message}</span>
+            )}
           </div>
           <div className={style.form__container}>
-            <span className={`tag16bold ${style["tag16bold--var"]}`}>
+            <p
+              className={`tag16bold ${style["tag16bold--var"]} ${style.form__titleInput}`}
+            >
               Confirme su contraseña
-            </span>
+            </p>
             <div className={style.form__containerInput}>
               <BiLock className={`icon ${style.icon} ${style["icon--left"]}`} />
               <input
                 type={unlockConfirmPassword ? "text" : "password"}
                 placeholder="********************"
                 className="form__input"
+                {...register("confirmPassword", {
+                  required: "*Este campo es requerido",
+                })}
               />
               {unlockConfirmPassword ? (
                 <BiShow
@@ -104,6 +316,9 @@ export const Register = () => {
                 />
               )}
             </div>
+            {errors.confirmPassword && (
+              <span className="danger">{errors.confirmPassword?.message}</span>
+            )}
           </div>
           <button
             className={`form__button boton14medium ${style["boton14medium--var"]}`}
