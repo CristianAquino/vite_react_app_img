236022f6c61195da568daefcddb109ea8538263b
cristianaquino <ycristian10@gmail.com>
2023-03-26 22:24:03 -0500

    feat: add and remove methods in cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,18 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { allfolders } from "../../https/folderRequest";
+import { folderSuccess } from "../../redux/slices/folderSlice";
 import Card from "../Card/Card";
 import style from "./Cards.module.css";
 
 const Cards = () => {
+  const { folders } = useSelector((state) => state.folderSlice);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    allfolders().then((res) => dispatch(folderSuccess(res)));
+  }, []);
+
+  if (!folders || folders.length === undefined)
+    return (
+      <span
+        className={`description14medium ${style["description14medium--var"]}`}
+      >
+        Loading...
+      </span>
+    );
+
   return (
     <div className={style.cards}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {folders?.map((card) => (
+        <Card key={card.id} data={card} />
+      ))}
     </div>
   );
 };
