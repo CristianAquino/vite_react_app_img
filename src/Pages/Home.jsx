80b5a6c5c548d1f1f4a06c328ba06d07e06eacf0
cristianaquino <ycristian10@gmail.com>
2023-03-26 22:25:57 -0500

    feat: adding hook in home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 import Cards from "../components/Cards/Cards";
 import Navigate from "../components/Navigate/Navigate";
 import Ranking from "../components/Ranking/Ranking";
+import { chapterSuccess } from "../redux/slices/chapterSlice";
 import style from "./Principal.module.css";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(chapterSuccess(null));
+  }, []);
+
   return (
     <div className={style.home}>
       <h1 className={`tag24bold ${style["tag24bold--var"]}`}>Recientes</h1>
