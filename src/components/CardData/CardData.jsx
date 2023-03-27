a112b4add793084a4479ebe1cf821becab4d7adf
cristianaquino <ycristian10@gmail.com>
2023-03-26 22:24:27 -0500

    feat: add and remove methods in cardData

--- src/components/CardData/CardData.jsx
+++ src/components/CardData/CardData.jsx
@@ -1,48 +1,41 @@
+import { Link } from "react-router-dom";
 import style from "./CardData.module.css";
 
-const CardData = () => {
-  const chapters = [
-    {
-      id: 1,
-      ch: "Ch. 101",
-      f: "13/01/2022",
-    },
-    {
-      id: 2,
-      ch: "Ch. 100",
-      f: "11/01/2022",
-    },
-  ];
+const CardData = ({ data }) => {
+  const { title, lastChapters } = data;
 
-  const hoy = new Date().getDate();
+  const handleTime = (chapter) => {
+    let hoy = new Date();
+    let apiTime = new Date(chapter);
+    let diferencia = hoy.getTime() - apiTime.getTime();
+    let days = Math.floor(diferencia / 1000 / 60 / 60);
+    return days;
+  };
 
   return (
     <div className={style.cardData}>
       <p
         className={`title16bold ${style["title16bold--var"]} ${style.cardData__title}`}
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-        ratione.
+        {title}
       </p>
-      {chapters.map((chapter) => {
-        const pub = chapter.f.split("/")[0];
+      {lastChapters.map((chapter) => {
+        const fecha = handleTime(chapter.createdAt);
         return (
-          <div className={style.chContainer} key={chapter.id}>
-            <p className={style.ch}>{chapter.ch}</p>
-            <p className={hoy <= pub ? style["ch--new"] : style["ch--date"]}>
-              {hoy <= pub ? "New" : chapter.f}
-            </p>
-          </div>
+          <Link
+            key={chapter.id}
+            to={`/view/${data.title}/${chapter.name}`}
+            className="link"
+          >
+            <div className={style.chContainer}>
+              <p className={style.ch}>{chapter.name}</p>
+              <p className={fecha <= 72 ? style["ch--new"] : style["ch--date"]}>
+                {fecha <= 72 ? "New" : chapter.createdAt.split("T")[0]}
+              </p>
+            </div>
+          </Link>
         );
       })}
-      {/* <div className={style.ch}>
-        <p className={style.first}>Ch.101</p>
-        <p className={style.second}>New</p>
-      </div> */}
-      {/* <div className={style.ch}>
-        <p className={style.first}>Ch.100</p>
-        <p className={style.date}>27/07/2022</p>
-      </div> */}
     </div>
   );
 };
